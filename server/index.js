e3b3a0f6f05583877fb0feddca459203cffb8217
itzhimangshu <itzhimangshu@gmail.com>
2024-06-16 15:57:14 +0530

    setting up MongoDB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => console.log("SERVER STARTED!"));
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to Database!");
+  app.listen(process.env.PORT, () => console.log("SERVER STARTED!"));
+}).catch((error) => {
+    console.log(error);
+});
