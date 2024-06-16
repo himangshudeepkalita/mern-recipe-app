1df53d5bc21e54482a99ea00f1ecd56c16adc83b
itzhimangshu <itzhimangshu@gmail.com>
2024-06-16 15:32:59 +0530

    setting up API server using Express and middleware

--- server/index.js
+++ server/index.js
@@ -3,3 +3,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
