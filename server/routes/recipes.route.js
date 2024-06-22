c308ee6039168e23303051f74832ebe5500cf4c8
itzhimangshu <itzhimangshu@gmail.com>
2024-06-23 00:12:39 +0530

    ui for recipes

--- server/routes/recipes.route.js
+++ server/routes/recipes.route.js
@@ -36,18 +36,18 @@ router.put("/", async (req, res) => {
     }
 });
 
-router.get("/savedRecipes/ids", async (req, res) => {
+router.get("/savedRecipes/ids/:userID", async (req, res) => {
     try {
-        const user = await UserModel.findById(req.body.userID);
+        const user = await UserModel.findById(req.params.userID);
         res.json({ savedRecipes: user?.savedRecipes });
     } catch (error) {
-        res.json(err);
+        res.json(error);
     }
 });
 
-router.get("/savedRecipes", async (req, res) => {
+router.get("/savedRecipes/:userID", async (req, res) => {
     try {
-        const user = await UserModel.findById(req.body.userID);
+        const user = await UserModel.findById(req.params.userID);
         const savedRecipes = await RecipeModel.find({
             _id: { $in: user.savedRecipes },
         });
